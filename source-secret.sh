23db56958da0e82f7ed37bccd941933a4d09764a
Francisco Delmar Kurpiel <francisco@kurpiel.eng.br>
2023-11-22 17:41:58 +0100

    Cleanup

--- source-secret.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-export TEST_CF_ZONE_NAME=simplesurance.top
-
-TEST_CF_APITOKEN=$(vault kv get --field=api_token covert/apps/xsell/nats-dns-manager-service/sisu-sandbox-eu/cloudflare)
-export TEST_CF_APITOKEN
